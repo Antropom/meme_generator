2915fc82c4bcebe30090294d2e997a2a1c078796
Anthony31400 <anthony.gensane@gmail.com>
2020-06-07 18:29:37 +0200

    transformation function app en class

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Sentence from './components/Sentence'
 import './App.css';
 
-function App() {
-  return <div className="App">
-    <Sentence />
-  </div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Sentence />
+      </div>
+    );
+  }
 }
 
 export default App;
