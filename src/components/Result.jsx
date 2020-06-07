d5910489802c0a10fdfa79259ea96313d8f79ce9
Louis <colletlouis@gmail.com>
2020-06-07 20:39:52 +0200

    Text display done

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,18 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Result = (props) => {
   const canvasRef = useRef(null);
+  const [image, setImage] = useState(null);
   const { topSentence, bottomSentence, selectedImg } = props;
 
   useEffect(() => {
-    const canvasObj = canvasRef.current;
-    const ctx = canvasObj.getContext('2d');
     const img = new Image();
     img.src = selectedImg;
-    canvasObj.height = img.height;
-    canvasObj.width = img.width;
-    ctx.drawImage(img, 0, 0, img.width, img.height);
-  }, [topSentence, bottomSentence, selectedImg]);
+    img.onload = () => setImage(img);
+  }, [selectedImg]);
+
+  useEffect(() => {
+    console.log(image, topSentence, bottomSentence);
+    if (!image) return;
+    const canvasObj = canvasRef.current;
+    const ctx = canvasObj.getContext('2d');
+    ctx.font = `24px impact`;
+    canvasObj.height = image.height;
+    canvasObj.width = image.width;
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+    ctx.fillStyle = 'black';
+    const top = Math.floor(image.height * 0.05);
+    const bottom = Math.floor(image.height * 0.8);
+    ctx.fillText(topSentence, 11, top + 1);
+    ctx.fillText(bottomSentence, 11, bottom + 1);
+    ctx.fillStyle = 'white';
+    ctx.fillText(topSentence, 10, top);
+    ctx.fillText(bottomSentence, 10, bottom);
+  }, [topSentence, bottomSentence, image]);
 
   return <canvas ref={canvasRef} />;
 };
