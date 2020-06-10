d94b9f852b957561f21d8ff9cb180bbf4268ed6d
Louis <colletlouis@gmail.com>
2020-06-10 20:15:04 +0200

    Little fix Noty

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
       .post(`${process.env.REACT_APP_API_URL}/api/memes`, body)
       .then((res) =>
         new Noty({
-          text: 'post effectué',
+          text: 'Post Effectué',
         }).show()
       )
       .then(() => {
