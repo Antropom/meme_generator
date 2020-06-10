1793074b8a7f36865af9418abe5b201ee97ef579
PaulCrtr <paul.th.cartier@gmail.com>
2020-06-10 19:45:56 +0200

    added cursor pointer

--- src/components/Sentence.jsx
+++ src/components/Sentence.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import './component.css'
+import React from "react";
+import "./component.css";
 
-function Sentence({ topSentence, setTopSentence, bottomSentence, setBottomSentence, handlesubmit }) {
-
-    return (
-        <div className="flex size field">
-                <h4>2) Merci de faire des memes drôles</h4>
-                <label className="small-title" htmlFor="">phrase du haut</label>
-                <input className="input" type="text" name="" id="" value={topSentence} onChange={e => setTopSentence(e.target.value)} />
-                <label className="small-title" htmlFor="">phrase du bas</label>
-                <input className="input" type="text" name="" id="" value={bottomSentence} onChange={e => setBottomSentence(e.target.value)} />
-                <div className="flex row">
-                    <button onClick={handlesubmit}>Envoyer</button>
-                    <button>Télécharger</button>
-                </div>
-                <h4>3) Partagez-le massivement</h4>
-        </div>
-    )
-
+function Sentence({
+  topSentence,
+  setTopSentence,
+  bottomSentence,
+  setBottomSentence,
+  handlesubmit,
+}) {
+  return (
+    <div className="flex size field">
+      <h4>2) Merci de faire des memes drôles</h4>
+      <label className="small-title" htmlFor="">
+        phrase du haut
+      </label>
+      <input
+        className="input"
+        type="text"
+        name=""
+        id=""
+        value={topSentence}
+        onChange={(e) => setTopSentence(e.target.value)}
+      />
+      <label className="small-title" htmlFor="">
+        phrase du bas
+      </label>
+      <input
+        className="input"
+        type="text"
+        name=""
+        id=""
+        value={bottomSentence}
+        onChange={(e) => setBottomSentence(e.target.value)}
+      />
+      <div className="flex row">
+        <button className="cursor-pointer" onClick={handlesubmit}>
+          Envoyer
+        </button>
+        <button className="cursor-pointer">Télécharger</button>
+      </div>
+      <h4>3) Partagez-le massivement</h4>
+    </div>
+  );
 }
 
 export default Sentence;
